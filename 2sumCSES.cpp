6635ca2cb609db7bb813882e22bf2fe8ce263eff
Nikhil <nikhilswayam16@gmail.com>
2026-07-20 10:36:49 +0000

    Two Sum from CSES

--- 2sumCSES.cpp
+++ 2sumCSES.cpp
@@ -2,6 +2,22 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+//There could be several rooie mistakes here.
+//First is that we may think that the answer is solvable 
+//using two directly . Even though,the core idea is true,
+//nonetheless, it still needs some tweaking.
+//First thing to notice here is that the problem input array
+//doesn't strictly follow monotonicity.
+//The values are not unique. 
+//So one would be tempted to use this thinking. Let's use
+//map. We will use values as keys and indices as values.
+//However, values aren't unique!
+//So, one would be tempted to use indices as keys
+//But again this is a trap! but we will be sorting the
+//array and original indices are going to be lost anyway!!!!!
+//why not store these indices as well?
+//But that would be lot of tedious error prone work.
+//Best option: use pair data structure.
 int main()
 {
 long long n,target;
